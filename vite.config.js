fba4042a6b8d508ad81ad7cdf915ff97c4dbe362
JustC4lm <farrelputrasetiawan0408@gmail.com>
2025-12-09 02:46:49 +0700

    4th commit fiks

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+    import { defineConfig } from 'vite';
+    import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/letter-from-awan",
-})
+    export default defineConfig({
+      plugins: [react()],
+      base: '/letter-from-awan/', // <--- Make sure this matches your deployment path
+      build: {
+        outDir: 'dist', // Default output directory
+        assetsDir: 'assets', // Default assets directory within outDir
+      },
+    });
+    
